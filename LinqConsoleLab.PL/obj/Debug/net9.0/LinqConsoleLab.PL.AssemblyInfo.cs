1bb471bce025ad0df544ffc24bd77db69148af64
Tymur Katalnikov <s33742@pjwstk.edu.pl>
2026-04-03 01:53:43 +0200

    feat: rozwiązano wszystkie zadania LINQ (1-20)

--- LinqConsoleLab.PL/obj/Debug/net9.0/LinqConsoleLab.PL.AssemblyInfo.cs
+++ LinqConsoleLab.PL/obj/Debug/net9.0/LinqConsoleLab.PL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinqConsoleLab.PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb367b8c4d068bb1697d5ff962882401a96d62aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinqConsoleLab.PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinqConsoleLab.PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
